f46d6e8f0944872bfc661bb3df82eeb9678381be
NIKITA VOLKOV <82369002+usepp0@users.noreply.github.com>
2022-03-22 16:45:51 +0300

    Add files via upload

---
+++ 6.50-60volkov/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace _6._50_60volkov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(359, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "6.51. Код, добавляемый в обработчик события ColumnClick";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(359, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "6.52. Код для нового класса ListViewColumSorter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(55, 89);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(359, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "6.53. Изменение цвета элементов в ListView";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(55, 118);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(359, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = " 6.54. Проверка настроек в реестре ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(55, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(359, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "6.55. Создание многострочной подсказки";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(55, 176);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(359, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = " 6.56. Определение элемента, вызывающего контекстное меню";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(55, 205);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(359, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = " 6.57. Автоматическое закрытие контекстного меню ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(55, 234);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(359, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = " 6.58. Показ подсказки над узлом TreeView";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(55, 264);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(359, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "6.59. Программное переключение на другую вкладку";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(55, 294);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(359, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = " 6.60. Установка фокуса на элементе управления ";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(542, 31);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 6.50-60volkov/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace _6._50_60volkov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Вставляем следующий код в событие ColumnClick для элемента ListView
+            // Определяем, является ли колонка, на которой щелкнули, уже
+            // отсортированной колонкой
+            if (e.Column == lvwColumnSorter.SortColumn)
+            {
+                // Переворачиваем направление сортировки
+                if (lvwColumnSorter.Order == SortOrder.Ascending)
+                {
+                    lvwColumnSorter.Order = SortOrder.Descending;
+                }
+                else
+                {
+                    lvwColumnSorter.Order = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                // Устанавливаем колонку для сортировки
+                // По умолчанию сортировка по возрастанию
+                lvwColumnSorter.SortColumn = e.Column;
+                lvwColumnSorter.Order = SortOrder.Ascending;
+            }
+            // Сортируем с новыми настройками
+            this.listView1.Sort();
+        }
+        /// <summary>
+        /// Этот класс реализует интерфейс IComparer.
+        /// </summary>
+        public class ListViewColumnSorter : IComparer
+        {
+            /// <summary>
+            /// Номер колонки, по которой проводится сортировка
+            /// </summary>
+            private int ColumnToSort;
+            /// <summary>
+            /// Порядок, в котором проводится сортировка (например 'Ascending')
+            /// </summary>
+            private SortOrder OrderOfSort;
+            /// <summary>
+            /// Объект, проводящий нечувствительную к регистру сортировку
+            /// </summary>
+            private CaseInsensitiveComparer ObjectCompare;
+            /// <summary>
+            /// Конструктор. Инициализирует различные элементы
+            /// </summary>
+            public ListViewColumnSorter()
+            {
+                // Инициализировать колонку значением 0
+                ColumnToSort = 0;
+                // Иниацилизировать порядок сортировки значением 'none'
+                OrderOfSort = SortOrder.None;
+                // Инициализировать объект CaseInsensitiveComparer
+                ObjectCompare = new CaseInsensitiveComparer();
+            }
+            /// <summary>
+            /// Этот метод унаследован от интерфейса IComparer. Он сравнивает
+            /// два переданных ему объекта, не обращая внимания на регистр
+            /// </summary>
+            /// <param name="x">Первый объект для сравнения</param>
+            /// <param name="y">Второй объект для сравнения</param>
+            /// <returns>Результат сравнения. '0' в случае равенства,
+            /// отрицательный если 'x' меньше 'y' и положительный
+            /// если 'x' больше 'y'</returns>
+            public int Compare(object x, object y)
+            {
+                int compareResult;
+                ListViewItem listviewX, listviewY;
+                // Преобразует объекты для сравнения в объекты ListViewItem
+                listviewX = (ListViewItem)x;
+                listviewY = (ListViewItem)y;
+                // Сравнивает 2 значения
+                compareResult = ObjectCompare.Compare(
+                listviewX.SubItems[ColumnToSort].Text,
+                listviewY.SubItems[ColumnToSort].Text);
+                // Подсчитывает возвращаемый результат, базируясь на результате
+                // сравнения
+                if (OrderOfSort == SortOrder.Ascending)
+                {
+                    // Выбрана сортировка по возрастанию, возвращаем результат
+                    // операции сравнения
+                    return compareResult;
+                }
+                else if (OrderOfSort == SortOrder.Descending)
+                {
+                    // Выбрана сортировка по убыванию, возвращаем результат
+                    // операции сравнения со знаком минус
+                    return (-compareResult);
+                }
+                else
+                {
+                    // Возвращаем 0, чтобы показать равенство
+                    return 0;
+                }
+            }
+            /// <summary>
+            /// Получает или возвращает номер колонки, к которой применять
+            /// сортировку (по умолчанию 0).
+            /// </summary>
+            public int SortColumn
+            {
+                set
+                {
+                    ColumnToSort = value;
+                }
+                get
+                {
+                    return ColumnToSort;
+                }
+            }
+            /// <summary>
+            /// Устанавливает порядок сортировки (например, 'Ascending').
+            /// </summary>
+            public SortOrder Order
+            {
+                set
+                {
+                    OrderOfSort = value;
+                }
+                get
+                {
+                    return OrderOfSort;
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            listView1.Items.Clear();
+            listView1.Items.Add("Алла");
+            listView1.Items.Add("София");
+            listView1.Items[0].UseItemStyleForSubItems = false;
+            listView1.Items[0].SubItems.Add("Пугачева", Color.Pink,
+            Color.Yellow, Font);
+            listView1.Items[1].UseItemStyleForSubItems = false;
+            listView1.Items[1].SubItems.Add("Ротару", Color.Teal,
+            Color.Violet, Font);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if ((int)Microsoft.Win32.Registry.GetValue(
+@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Ad
+vanced", "EnableBalloonTips", 1) == 0) ;
+            // Не использовать стиль Balloon
+            this.Text = "Not use Balloon style";
+ else
+                this.toolTip1.IsBalloon = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.toolTip1.SetToolTip(this.button1,
+ "Это кнопка\r\nСамая обычная кнопка");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+            {
+                if (contextMenuStrip1.SourceControl == label1)
+                {
+                    cmenuOpen.Text = "Label";
+                }
+                else
+                    cmenuOpen.Text = "Button";
+            }
+        }
+        private void timerMenu_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send("{ESC}");
+            timerMenu.Stop();
+        }
+        private void contextMenuStrip1_Opened(object sender, EventArgs e)
+        {
+            //set interval to 5 seconds
+            timerMenu.Interval = 5000;
+            timerMenu.Start();
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            // Create a root node.
+            TreeNode rootNode = treeView1.Nodes.Add("Коты");
+            TreeNode childNode = rootNode.Nodes.Add("Барсик");
+            childNode.Tag = "Барсик - большой и умный кот";
+            childNode = rootNode.Nodes.Add("Рыжик");
+            childNode.Tag = "Рыжик - очень любопытный кот";
+            childNode = rootNode.Nodes.Add("Мурзик");
+            childNode.Tag = "Мурзик - ленивый кот";
+            childNode = rootNode.Nodes.Add("Пушок");
+            childNode.Tag = "Пушок - белый и пушистый кот";
+            // Раскрываем все узлы дерева
+            rootNode.ExpandAll();
+        }
+        private void treeView1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Получим узел в текущей позиции мыши
+            TreeNode theNode = this.treeView1.GetNodeAt(e.X, e.Y);
+            // Установим ToolTip, только если мышь задержалась на узле
+            if ((theNode != null))
+            {
+                // Проверяем, что свойство tag не пустое
+                if (theNode.Tag != null)
+                {
+                    // Меняем ToolTip, если мышь переместилась на другой узел
+                    if (theNode.Tag.ToString() !=
+                    this.toolTip1.GetToolTip(this.treeView1))
+                    {
+                        this.toolTip1.SetToolTip(this.treeView1,
+                        theNode.Tag.ToString());
+                    }
+                }
+                else
+                {
+                    this.toolTip1.SetToolTip(this.treeView1, "");
+                }
+            }
+            else // Если указатель не над узлом, то очистим подсказку
+            {
+                this.toolTip1.SetToolTip(this.treeView1, "");
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            // Переключаемся на другую вкладку при помощи SelectedTab
+            this.tabControl1.SelectedTab = this.tabPage2;
+            // Переключаемся на другую вкладку при помощи SelectedIndex
+            this.tabControl1.SelectedIndex = 0;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // не работает
+            this.button1.Focus();
+        }
+    }
+}
